d5a744d7cb88d3e2538402560026a83d8dcd8d12
Brian Matzon <brian@matzon.dk>
2016-03-06 14:37:38 +0000

    added repository methods

--- src/main/java/dk/matzon/echelog/domain/model/ChannelRepository.java
+++ src/main/java/dk/matzon/echelog/domain/model/ChannelRepository.java
@@ -1,4 +1,9 @@
 package dk.matzon.echelog.domain.model;
 
+import java.util.List;
+
 public interface ChannelRepository {
+  public List<Channel> findAll();
+  public List<Channel> findByNetwork(String _network);
+  public Channel findByNetworkAndName(String _network, String _name);
 }
